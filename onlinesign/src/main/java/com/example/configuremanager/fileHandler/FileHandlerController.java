43d2289b56ea5338b6584d7ed6965d960bad6a61
liumengbing <liumengbing@360fdc.com>
2018-10-17 16:43:45 +0800

    自己动手完整实现在线签约系统

---
+++ onlinesign/src/main/java/com/example/configuremanager/fileHandler/FileHandlerController.java
@@ -0,0 +1,172 @@
+package com.example.configuremanager.fileHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
+/**
+ * Created by lmb on 2018/10/17 0017.
+ *
+ * 文件上传下载处理类：
+ * 1、文件上传时使用了MultipartFile
+ * 2、文件下载的原理：读取下载目录文件的文件流，写入到指定目录
+ *
+ */
+//@RestController
+@Controller
+public class FileHandlerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileHandlerController.class);
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "pages/fileHandler.html";
+    }
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @RequestMapping(value = "/upload")
+    @ResponseBody
+    public String upload(@RequestParam("test") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "文件为空";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        logger.info("上传的文件名为：" + fileName);
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        logger.info("上传的后缀名为：" + suffixName);
+        // 文件上传后的路径
+        String filePath = "E://test//";
+        // 解决中文问题，liunx下中文路径，图片显示问题
+        // fileName = UUID.randomUUID() + suffixName;
+        File dest = new File(filePath + fileName);
+        // 检测是否存在目录
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            //transferTo（dest）方法将需要上传的文件写到服务器上指定的文件
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
+    /**
+     * 文件下载
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("/download")
+    public String downloadFile(org.apache.catalina.servlet4preview.http.HttpServletRequest request, HttpServletResponse response){
+        String fileName = "合同表contract.txt";
+        if (fileName != null) {
+            //当前是从该工程的WEB-INF//File//下获取文件(该目录可以在下面一行代码配置)然后下载到C:\\users\\downloads即本机的默认下载的目录
+//            String realPath = request.getServletContext().getRealPath(
+//                    "//WEB-INF//");
+            /*
+            String realPath = "E://test//";
+            报错：文件拒绝访问，解决办法
+            https://blog.csdn.net/zuihongyan518/article/details/81589559
+             */
+            String realPath = "E://test//合同表contract.txt";
+            File file = new File(realPath);
+            System.out.print(file.exists());
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition",
+                        "attachment;fileName=" +  fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("success");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 多文件上传
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/batch/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public String handleFileUpload(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request)
+                .getFiles("file");
+        /*文件上传后的路径
+        1、此处不给出路径，将上传到项目的根目录（和pom.xml同目录）
+        2、给出路径，将上传到指定路径
+         */
+        String filePath = "E://test//";
+        MultipartFile file = null;
+        BufferedOutputStream stream = null;
+        for (int i = 0; i < files.size(); ++i) {
+            file = files.get(i);
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    stream = new BufferedOutputStream(new FileOutputStream(
+                            new File(filePath + file.getOriginalFilename())));
+                    stream.write(bytes);
+                    stream.close();
+
+                } catch (Exception e) {
+                    stream = null;
+                    return "You failed to upload " + i + " => "
+                            + e.getMessage();
+                }
+            } else {
+                return "You failed to upload " + i
+                        + " because the file was empty.";
+            }
+        }
+        return "upload successful";
+    }
+}
