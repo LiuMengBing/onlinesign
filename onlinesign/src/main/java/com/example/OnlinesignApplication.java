8f65306b8c2aaba2ed5217cea79fb45d45f8378d
liumengbing <liumengbing@360fdc.com>
2018-10-17 16:41:55 +0800

    自己动手完整实现在线签约系统

--- onlinesign/src/main/java/com/example/OnlinesignApplication.java
+++ onlinesign/src/main/java/com/example/OnlinesignApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class OnlinesignApplication {
 
 	public static void main(String[] args) {
--- onlinesign/src/test/java/com/example/demo/OnlinesignApplicationTests.java
+++ onlinesign/src/test/java/com/example/demo/OnlinesignApplicationTests.java
@@ -1,16 +1,9 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class OnlinesignApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
