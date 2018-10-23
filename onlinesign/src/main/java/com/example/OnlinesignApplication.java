f0fcf9097b1e62d7d61a18fa815591ae41100e78
liumengbing <liumengbing@360fdc.com>
2018-10-23 19:04:04 +0800

    增加swagger2功能

--- onlinesign/src/main/java/com/example/OnlinesignApplication.java
+++ onlinesign/src/main/java/com/example/OnlinesignApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebSocket
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableSwagger2
 public class OnlinesignApplication {
 
 	public static void main(String[] args) {
