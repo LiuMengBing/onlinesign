dee127c9a3aa3701c798d10cabb891d56231411a
liumengbing <liumengbing@360fdc.com>
2018-10-24 17:33:19 +0800

    增加整合logback功能

--- onlinesign/src/main/java/com/example/OnlinesignApplication.java
+++ onlinesign/src/main/java/com/example/OnlinesignApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableSwagger2
+@MapperScan("com.example.*.dao")//项目中对应的mapper类的扫描路径
 public class OnlinesignApplication {
 
 	public static void main(String[] args) {
