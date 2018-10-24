fa2618a4cbbcbc70f9577f926ec8f9bee63c8097
liumengbing <liumengbing@360fdc.com>
2018-10-24 17:57:47 +0800

    1、整合mybatis功能 2、增加mybatis generator生成代码功能

---
+++ onlinesign/src/main/java/com/example/generator/bean/Messgae.java
@@ -0,0 +1,115 @@
+package com.example.generator.bean;
+
+import java.util.Date;
+
+public class Messgae {
+    private Integer id;
+
+    private String msgModule;
+
+    private String msgType;
+
+    private String msgContent;
+
+    private String msgStatus;
+
+    private String sendTo;
+
+    private Date sendTime;
+
+    private String sendor;
+
+    private Date updateTime;
+
+    private String updator;
+
+    private Byte isDel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMsgModule() {
+        return msgModule;
+    }
+
+    public void setMsgModule(String msgModule) {
+        this.msgModule = msgModule == null ? null : msgModule.trim();
+    }
+
+    public String getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(String msgType) {
+        this.msgType = msgType == null ? null : msgType.trim();
+    }
+
+    public String getMsgContent() {
+        return msgContent;
+    }
+
+    public void setMsgContent(String msgContent) {
+        this.msgContent = msgContent == null ? null : msgContent.trim();
+    }
+
+    public String getMsgStatus() {
+        return msgStatus;
+    }
+
+    public void setMsgStatus(String msgStatus) {
+        this.msgStatus = msgStatus == null ? null : msgStatus.trim();
+    }
+
+    public String getSendTo() {
+        return sendTo;
+    }
+
+    public void setSendTo(String sendTo) {
+        this.sendTo = sendTo == null ? null : sendTo.trim();
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(Date sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public String getSendor() {
+        return sendor;
+    }
+
+    public void setSendor(String sendor) {
+        this.sendor = sendor == null ? null : sendor.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdator() {
+        return updator;
+    }
+
+    public void setUpdator(String updator) {
+        this.updator = updator == null ? null : updator.trim();
+    }
+
+    public Byte getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Byte isDel) {
+        this.isDel = isDel;
+    }
+}
